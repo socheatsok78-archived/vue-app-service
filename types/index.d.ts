ea0bce95d454786d5a7208cd17483712f08ddef7
Socheat Sok <alex@socheat.net>
2020-05-26 12:55:25 +0700

    feat(docs): add type decoration

---
+++ types/index.d.ts
@@ -0,0 +1,99 @@
+import { VueConstructor } from "vue/types";
+import { AxiosStatic, AxiosRequestConfig } from "axios/index";
+
+export type ServiceRegistryOptions = {
+    name: string,
+    description: string,
+    config: AxiosRequestConfig,
+    methods: ServiceMethods
+}
+
+export type ServiceMethods = {
+    [key: string]: ServiceMethod
+}
+
+export type ServiceMethod = {
+    options: ServiceMethodOptions,
+    payload: <T>() => T
+}
+
+export type ServiceMethodOptions = {
+    $axios: AxiosStatic,
+    $config: AxiosRequestConfig
+}
+
+export default class Application {
+    /**
+     * Application name
+     */
+    name: string;
+
+    /**
+     * Application description
+     */
+    description: string;
+
+    /**
+     * Create new Application instance
+     * @param {AxiosRequestConfig} config
+     */
+    constructor(config: AxiosRequestConfig);
+
+    /**
+     * @returns {ServiceContainer} ServiceContainer
+     */
+    get $service(): ServiceContainer;
+}
+
+export class ServiceContainer {
+    /**
+     * Create new ServiceContainer instance
+     * @param {Application} app
+     */
+    constructor(app: Application);
+
+    /**
+     * Register a new service
+     * @param {ServiceRegistry} service Service Registry Instance
+     */
+    register(service: ServiceRegistry): void;
+
+    /**
+     * Unregister a service
+     * @param {string} name Service name
+     */
+    unregister(name: string): void;
+}
+
+export class ServiceRegistry {
+    /**
+     * Service name
+     */
+    name: string;
+
+    /**
+     * Service description
+     */
+    description: string;
+
+    /**
+     * Service methods
+     */
+    methods: ServiceMethods;
+
+    /**
+     * Create a new ServiceRegistry instance
+     * @param {ServiceRegistryOptions} service
+     */
+    constructor(service: ServiceRegistryOptions);
+
+    /**
+     * @returns {AxiosStatic} Request instance
+     */
+    get $http(): AxiosStatic;
+
+    /**
+     * @returns {AxiosRequestConfig} Request config
+     */
+    get $config(): AxiosRequestConfig;
+}
