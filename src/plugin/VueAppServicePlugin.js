1eebbce5fdf85eed554fd1ae8530cd1a330e64af
Socheat Sok <alex@socheat.net>
2020-05-30 16:43:13 +0700

    fix(core): add installed status for Vue plugin

--- src/plugin/VueAppServicePlugin.js
+++ src/plugin/VueAppServicePlugin.js
@@ -1,11 +1,16 @@
 import VueConstructor from "vue";
 
 export default class VueAppServicePlugin {
+    static installed = false
+
     /**
      * Install Vue App Service Plugin
      * @param {VueConstructor} Vue
      */
     static install(Vue) {
+        if (this.installed) return
+        this.installed = true
+
         Object.defineProperty(Vue.prototype, '$app', {
             enumerable: true,
             configurable: true,
